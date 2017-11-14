07181fd82253bb76b01ccea86aa6d01778738f0b
Patrick Sonderegger <patrick.sonderegger@uzh.ch>
2017-11-14 23:30:24 +0100

    Fixed solver

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -51,23 +51,29 @@ bool Sudoku::setValue(int pos, int value) { // insert new value and check validi
 		case 0 :
 			subMinX = x;
 			subMaxX = x+2;
+			break;
 		case 1 :
 			subMinX = x-1;
 			subMaxX = x+1;
+			break;
 		case 2:
 			subMinX = x-2;
 			subMaxX = x;
+			break;
 	}
 	switch(y%3) {
 		case 0:
 			subMinY = y;
 			subMaxY = y+2;
+			break;
 		case 1:
 			subMinY = y-1;
 			subMaxY = y+1;
+			break;
 		case 2:
 		subMinY = y-2;
 		subMaxY = y;
+		break;
 	}
 	// Check sub block
 	for(int i=subMinX; i<=subMaxX; i++) {
