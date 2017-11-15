db23268111d46965fdfde3ac06a89c513d032923
Patrick Sonderegger <patrick.sonderegger@uzh.ch>
2017-11-15 10:50:15 +0100

    Now taking input from the user rather than hard coding the puzzle

--- sudoku_main.cpp
+++ sudoku_main.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 #include <list>
+#include <string>
 #include "Sudoku.h"
 
+void readSudoku(int A[9][9]) {
+  std::cout << "Please enter your 9x9 Sudoku by entering its numbers separated by spaces, use 0 for blank fields:\n";
+	for(int i=0; i<9; i++) {
+		for(int j=0; j<9; j++) {
+			std::cin >> A[i][j];
+		}
+	}
+}
+
 int main() {
-	int A[9][9] = {{0,4,9, 0,0,8, 0,0,0},
-	               {0,0,0, 0,2,0, 0,0,7},
-		       {0,3,0, 0,0,0, 0,8,0},
 
-		       {0,0,0, 3,0,9, 0,6,8},
-		       {0,0,0, 0,8,0, 9,0,4},
-		       {0,5,0, 0,0,6, 0,0,0},
-		       
-		       {0,6,0, 0,0,3, 0,2,0},
-		       {5,0,0, 0,0,0, 7,9,6},
-		       {4,0,0, 0,0,5, 0,0,0}};
-
-  Sudoku initial(A);
+	int A[9][9];
 	std::list<Sudoku> L = {};
-	L.push_front(initial);
+	std::list<Sudoku> Solutions = {};
+	while(L.empty()) {
+		readSudoku(A);
+		Sudoku init(A);
+		std::cout << "\nYou have entered:\n";
+		init.printSudoku();
+		std::cout << "Is this correct (y/n)? ";
+		char answer;
+		std::cin >> answer;
+		if(answer == 'y') {
+			L.push_front(init);
+		}
+	}
+
 	while(!L.empty()) {
 		Sudoku t = L.front();
 		L.pop_front();
@@ -30,7 +42,25 @@ int main() {
 			}
 		}
 		else {
-			t.printSudoku();
+			Solutions.push_back(t);
 		}
+	}
+
+	int solutionsCount = Solutions.size();
+	std::string output;
+	switch(solutionsCount) {
+		case 0:
+			output = "No solutions found \n";
+			break;
+		case 1:
+			output = "1 solution found:\n\n";
+			break;
+		default:
+			output = std::to_string(solutionsCount) + " solutions found: \n\n";
+	}
+	std::cout << output;
+	while(!Solutions.empty()) {
+		Solutions.front().printSudoku();
+		Solutions.pop_front();
 	}
 }
